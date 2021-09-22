9f5502069b0081f2384057ff0613cde5ebaee43d
Jose Limbert <jose.jl@gmail.com>
2021-09-22 16:44:14 -0400

    quitar comentarios

--- server/config/config.js
+++ server/config/config.js
@@ -8,10 +8,10 @@ process.env.NODE_ENV=process.env.NODE_ENV||'dev';
 
 //base de datos
 let urlBD;
-// if(process.env.NODE_ENV==='dev')
-// {
-    // urlBD='mongodb://localhost:27017/cafe'
-// }else{
+if(process.env.NODE_ENV==='dev')
+{
+    urlBD='mongodb://localhost:27017/cafe'
+}else{
     urlBD='mongodb+srv://root:1234jose@cluster0.rwkz9.mongodb.net/cafe?retryWrites=true&w=majority'
-// }
+}
 process.env.urlbase=urlBD
